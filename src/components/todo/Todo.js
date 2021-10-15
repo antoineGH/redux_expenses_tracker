20e471a585fcdca237302bd4eedbc41c271f72c2
Templars <antoine.ratat@gmail.com>
2021-10-16 01:58:38 +0800

    update design todo

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Switch, Button } from 'antd'
+import { Row, Col, Card, Switch, Button } from 'antd'
 import { CloseOutlined } from '@ant-design/icons'
 
 export default function Todo(props) {
@@ -24,22 +24,33 @@ export default function Todo(props) {
 						icon={<CloseOutlined />}></Button>
 				}
 				style={{ width: 300 }}>
-				{todo.length >= 1 && (
-					<p className='text-capitalize'>
-						{user['first_name']} {user['last_name']}
-					</p>
+				{user && (
+					<Row>
+						<Col span={24}>
+							<p className='text-capitalize'>
+								{user['first_name']} {user['last_name']}
+							</p>
+						</Col>
+					</Row>
 				)}
+
 				<p>Todo n°{count}</p>
-				<p>Status</p>
-				<Switch
-					id='completed'
-					name='completed'
-					onChange={() =>
-						handleToggleTodo(todo.todo_id, !todo.completed)
-					}
-					defaultChecked={todo.completed}
-					disabled={isLoadingToggleTodo}
-				/>
+				<Row justify='center'>
+					<Col span={5}>
+						<p>Status:</p>
+					</Col>
+					<Col span={5}>
+						<Switch
+							id='completed'
+							name='completed'
+							onChange={() =>
+								handleToggleTodo(todo.todo_id, !todo.completed)
+							}
+							defaultChecked={todo.completed}
+							disabled={isLoadingToggleTodo}
+						/>
+					</Col>
+				</Row>
 			</Card>
 		</>
 	)
