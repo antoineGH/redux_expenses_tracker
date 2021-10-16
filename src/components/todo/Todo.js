385d171d2ad8bdc40d3706546772a29b6c863d6a
Templars <antoine.ratat@gmail.com>
2021-10-16 23:59:43 +0800

    update todo design

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,45 +1,52 @@
 import React from 'react'
-import { Row, Col, Card, Switch, Button } from 'antd'
+import { Row, Col, Card, Switch, Button, Typography } from 'antd'
 import { CloseOutlined } from '@ant-design/icons'
 
 export default function Todo(props) {
 	const {
 		todo,
 		user,
-		count,
 		handleDeleteTodo,
 		handleToggleTodo,
 		isLoadingDelete,
 		isLoadingToggleTodo,
 	} = props
+	const { Text, Title } = Typography
 
 	return (
 		<>
-			<Card
-				title={todo.todo_description}
-				extra={
-					<Button
-						onClick={() => handleDeleteTodo(todo.todo_id)}
-						loading={isLoadingDelete}
-						icon={<CloseOutlined />}></Button>
-				}
-				style={{ width: 300 }}>
+			<Card className='todoCard' style={{ width: 300 }}>
+				<Row>
+					<Col span={22} className='col-titleTodo'>
+						<Title level={4} className='text-capitalize'>
+							{todo.todo_description}
+						</Title>
+					</Col>
+					<Col span={1} className='col-btnClose'>
+						<Button
+							className='btn-closetodo'
+							onClick={() => handleDeleteTodo(todo.todo_id)}
+							loading={isLoadingDelete}
+							icon={<CloseOutlined />}></Button>
+					</Col>
+				</Row>
+				<Row></Row>
+
 				{user && (
 					<Row>
-						<Col span={24}>
-							<p className='text-capitalize'>
-								{user['first_name']} {user['last_name']}
-							</p>
+						<Col span={20} className='col-userTodo'>
+							<Text
+								className='text-capitalize'
+								style={{
+									fontSize: '.9rem',
+								}}>
+								for {user['first_name']} {user['last_name']}
+							</Text>
 						</Col>
 					</Row>
 				)}
-
-				<p>Todo n°{count}</p>
-				<Row justify='center'>
-					<Col span={5}>
-						<p>Status:</p>
-					</Col>
-					<Col span={5}>
+				<Row>
+					<Col span={24} className='col-switchTodo'>
 						<Switch
 							id='completed'
 							name='completed'
